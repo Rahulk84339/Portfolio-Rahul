bb2aefd0947713868cd7149a05293f5bb9e971a3
Rahulk8439 <rahul.sharma20030158@gmail.com>
2021-08-16 14:23:25 +0530

    skills, qualification, img, portfolio update

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,18 +50,18 @@ skillsHeader.forEach((el) => {
 
 /*==================== QUALIFICATION TABS ====================*/
 const tabs = document.querySelectorAll("[data-target]"),
-  tabContents = document.querySelectorAll("[data-content]");
+      tabContents = document.querySelectorAll("[data-content]");
 
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
+tabs.forEach(tab => {
+  tab.addEventListener("click", () =>{
     const target = document.querySelector(tab.dataset.target);
 
-    tabContents.forEach((tabContent) => {
+    tabContents.forEach(tabContent =>{
       tabContent.classList.remove("qualification__active");
     });
     target.classList.add("qualification__active");
 
-    tabs.forEach((tab) => {
+    tabs.forEach(tab =>{
       tab.classList.remove("qualification__active");
     });
     tab.classList.add("qualification__active");
